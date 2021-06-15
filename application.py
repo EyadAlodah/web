e7e2c692bf6afe5ade7ad63e1284aa9052126bea
EyadAlodah <eyad.alodah1@gmail.com>
2021-06-15 14:27:04 +0300

    changing the port number

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ from FlaskTemplate import app
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '3000'))
+        PORT = int(environ.get('SERVER_PORT', '3001'))
     except ValueError:
-        PORT = 3000
+        PORT = 3001
     app.run(HOST, PORT)
